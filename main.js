838de267040db8a52edb1897f121ce831b604a17
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-11-18 16:50:22 -0500

    break

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 window.onload = function(){
   console.log('DOM loaded');
 
-var socialRequest = new XMLHttpRequest();
-  socialRequest.open('GET', 'http://192.168.0.6:8080/posts.json');
-  socialRequest.onload = function() {
-  console.log(socialRequest.responseText);
-    var socialData = socialRequest.responseText;
-    console.log(socialData[0]);
-};
+var socialDiv = document.getElementById('social');
+var button = document.getElementById('button');
+  button.addEventListener('click', function() {
 
-socialRequest.send();
+  // I did edit the json file, it had errors in a few lines.
+  var socialRequest = new XMLHttpRequest();
+    socialRequest.open('GET', 'http://192.168.0.6:8080/posts.json');
+    socialRequest.onload = function() {
+    // console.log(socialRequest.responseText);
+      var socialData = JSON.parse(socialRequest.responseText);
+      // console.log(socialData[0]);
+      appendSocial(socialData);
+  };
+  socialRequest.send();
+
+});
+function appendSocial(data) {
+  var html = "";
+    for (var i = 0; i < data.length; i++) {
+      htmlString += "<p>" + data[i].items + data[i].items_id.text + data[i].items_id +"</p>";
+    }
+
+    socialDiv.document.getElementById("social").innerHTML = "Hello !";
+  // socialDiv.insertAdjacentHTML('beforeend', 'html');
 
 
+
+}
 
 
 
