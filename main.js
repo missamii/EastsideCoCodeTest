25627df81428646a7c0f369870df97d9a8c78847
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-11-21 15:12:06 -0500

    ugh finally got it

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
 window.onload = function(){
   console.log('DOM loaded');
 
-// var loadMore = "";
-var socialDiv = document.getElementById('social');
-var button = document.getElementById('button');
-  button.addEventListener('click', function() {
+  $("button").click( function(){
+  $.getJSON("http://192.168.0.6:8080/posts.json", function(data) {
+    $.each(data.items, function(items, info) {
+      // console.log(info);
+      $("div").append("<p>" + info.item_data.text + "</p>" + "<a href=" + info.item_data.link + ">" + "</a>");
 
-  // I did edit the json file, it had errors in a few lines.
-  var socialRequest = new XMLHttpRequest();
-    socialRequest.open('GET', 'http://192.168.0.6:8080/posts.json');
-    socialRequest.onload = function() {
-    // console.log(socialRequest.responseText);
-      var socialData = JSON.parse(socialRequest.responseText);
-      // console.log(socialData[0]);
-      appendSocial(socialData);
-  };
-  socialRequest.send();
-
-});
-function appendSocial(data) {
-  var html = "";
-console.log(data);
-    for (var i = 0; i < data.length; i++) {
-      html += "<p>" + data[i].items + data[i].items_id.text + data[i].items_id +"</p>";
-    }
-
-    // socialDiv.document.getElementById("social").innerHTML = "Hello !";
-  socialDiv.insertAdjacentHTML('beforeend', 'html');
-
-
-
-}
-
-
-
+    }); //end loop
+  }); //end ajax
+  } //end button on click
+);
 }; //end window onload
